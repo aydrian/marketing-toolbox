4e4183d0d2c7c6dae30b556b1ded02ce3e5efdd5
aydrian <aydrian@gmail.com>
2023-12-12 15:09:44 -0500

    Initial commit from create-remix

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+import { flatRoutes } from 'remix-flat-routes'
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ['**/*'],
+  serverModuleFormat: 'esm',
+  routes: async defineRoutes => {
+    return flatRoutes('routes', defineRoutes, {
+      ignoredRouteFiles: ['**/*.test.{js,jsx,ts,tsx}', '**/__*.*'],
+    })
+  },
+}
